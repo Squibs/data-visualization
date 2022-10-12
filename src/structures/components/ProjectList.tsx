24ce9a009a2857ee20272ba1d8fd37d4654d8aeb
Squibs <z.r.holman+github@gmail.com>
2022-10-12 04:21:44 -0600

    ProjectList(dropdown) working.

---
+++ src/structures/components/ProjectList.tsx
@@ -0,0 +1,117 @@
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'twin.macro';
+import { default as twColors } from 'tailwindcss/colors';
+import { BarChart, ScatterplotGraph, HeatMap, ChoroplethMap, TreemapDiagram } from './charts';
+import HomePage from './HomePage';
+
+/* ---------------------------------- types --------------------------------- */
+
+type IsLargeScreenProp = { isLargeScreen?: boolean };
+type NavLinkProps = { firstOrLast: string } & IsLargeScreenProp;
+
+type NavArrayLayout = [[number, string, string, ReactNode]];
+
+type ProjectListProps = {
+  updateCurrentGraph: ({ project, title }: { project: ReactNode; title: string }) => void;
+  currentGraph: string;
+} & IsLargeScreenProp;
+
+/* --------------------------------- styles --------------------------------- */
+
+const StyledNav = styled.nav<IsLargeScreenProp>`
+  ${tw`inline-block relative`}
+
+  .active {
+    background-color: ${twColors.gray[400]}; // bg-gray-400
+  }
+
+  ${({ isLargeScreen }) => isLargeScreen && tw`w-full`}
+`;
+
+const NavButton = tw.button`bg-gray-300 text-gray-700 font-semibold py-2 px-3 rounded inline-flex items-center mr-0.5`;
+
+const NavChevron = tw.svg`fill-current h-4 w-4`;
+
+const NavListContainer = styled.ul<IsLargeScreenProp>`
+  ${tw`absolute hidden text-gray-700 pt-0.5`}
+
+  ${({ isLargeScreen }) => isLargeScreen && tw`relative grid grid-cols-3 grid-rows-2 w-full `}
+`;
+
+// when the css has more logic than anything else
+const NavLink = styled.button<NavLinkProps>`
+  /* rounded corners on top and bottom */
+  ${({ firstOrLast }) =>
+    ['0', '5'].includes(firstOrLast) && firstOrLast === '0' ? tw`rounded-t` : tw`rounded-b`}
+  ${tw`py-2 px-4 block whitespace-nowrap w-full [text-align: left] bg-gray-200 hover:bg-gray-400 focus:bg-gray-400`}
+
+  /* if the element is for large screens */
+  ${({ isLargeScreen, firstOrLast }) => {
+    if (isLargeScreen) {
+      return tw`text-center`;
+    }
+  }}
+`;
+
+/* -------------------------------- component ------------------------------- */
+
+const ProjectList = ({
+  updateCurrentGraph,
+  currentGraph,
+  isLargeScreen = false,
+}: ProjectListProps) => {
+  const [expandNav, setExpandNav] = useState(false);
+
+  return (
+    <StyledNav
+      isLargeScreen={isLargeScreen}
+      onBlur={(e) => (e.currentTarget.contains(e.relatedTarget) ? null : setExpandNav(false))}
+    >
+      {!isLargeScreen && (
+        <NavButton type="button" onClick={() => setExpandNav((p) => !p)}>
+          <span tw="mr-1">Select a Chart:</span>
+          <NavChevron xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </NavChevron>
+        </NavButton>
+      )}
+      <NavListContainer
+        isLargeScreen={isLargeScreen}
+        css={css`
+          ${expandNav ? 'display: block' : 'display: hidden'}
+        `}
+      >
+        {(
+          [
+            [0, 'Home', '#', HomePage],
+            [1, 'Bar Chart', '#bar-chart', BarChart],
+            [2, 'Scatterplot Graph', '#scatterplot-graph', ScatterplotGraph],
+            [3, 'Heat Map', '#heat-map', HeatMap],
+            [4, 'Choropleth Map', '#choropleth-map', ChoroplethMap],
+            [5, 'Treemap Diagram', '#treemap-diagram', TreemapDiagram],
+          ] as unknown as NavArrayLayout
+        ).map(([index, title, url, project]) => (
+          <li key={index}>
+            <NavLink
+              isLargeScreen={isLargeScreen}
+              type="button"
+              firstOrLast={index.toString()}
+              className={currentGraph === title ? 'active' : ''}
+              onClick={(e) => {
+                updateCurrentGraph({ project, title });
+                setExpandNav(false);
+              }}
+            >
+              {title}
+            </NavLink>
+          </li>
+        ))}
+      </NavListContainer>
+    </StyledNav>
+  );
+};
+
+/* -------------------- default props / queries / exports ------------------- */
+
+export default ProjectList;
