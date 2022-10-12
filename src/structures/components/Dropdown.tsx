f03394c666936f2a748bdd2338a080afd1a1e7a5
Squibs <z.r.holman+github@gmail.com>
2022-10-11 22:36:22 -0600

    Dropdown selector for graphs.

---
+++ src/structures/components/Dropdown.tsx
@@ -0,0 +1,86 @@
+import React, { ReactNode, useState } from 'react';
+import styled, { css } from 'styled-components';
+import tw from 'twin.macro';
+import BarChart from './BarChart';
+import ChoroplethMap from './ChoroplethMap';
+import HeatMap from './HeatMap';
+import HomePage from './HomePage';
+import ScatterplotGraph from './ScatterplotGraph';
+import TreemapDiagram from './TreemapDiagram';
+
+/* ---------------------------------- types --------------------------------- */
+
+type NavLinkProps = { firstOrLast: string };
+type NavArrayLayout = [[number, string, string, ReactNode]];
+
+type DropdownProps = {
+  updateCurrentGraph: ({ project, title }: { project: ReactNode; title: string }) => void;
+};
+
+/* --------------------------------- styles --------------------------------- */
+
+const StyledNav = tw.nav`inline-block relative`;
+
+const NavButton = tw.button`bg-gray-300 text-gray-700 font-semibold py-2 px-3 rounded inline-flex items-center`;
+
+const NavChevron = tw.svg`fill-current h-4 w-4`;
+
+const NavListContainer = tw.ul`absolute hidden text-gray-700 pt-0.5`;
+
+const NavLink = styled.a<NavLinkProps>`
+  ${({ firstOrLast }) => (firstOrLast === '0' ? tw`rounded-t` : null)}
+  ${({ firstOrLast }) => (firstOrLast === '5' ? tw`rounded-b` : null)}
+  ${tw`py-2 px-4 block whitespace-nowrap bg-gray-200 hover:bg-gray-400`}
+`;
+
+/* -------------------------------- component ------------------------------- */
+
+const Dropdown = ({ updateCurrentGraph }: DropdownProps) => {
+  const [expandNav, setExpandNav] = useState(false);
+
+  return (
+    <StyledNav
+      onBlur={(e) => (e.currentTarget.contains(e.relatedTarget) ? null : setExpandNav(false))}
+    >
+      <NavButton type="button" onClick={() => setExpandNav((p) => !p)}>
+        <span tw="mr-1">Select a Chart:</span>
+        <NavChevron xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </NavChevron>
+      </NavButton>
+      <NavListContainer
+        css={css`
+          ${expandNav ? 'display: block' : 'display: hidden'}
+        `}
+      >
+        {(
+          [
+            [0, 'Home', '#', HomePage],
+            [1, 'Bar Chart', '#bar-chart', BarChart],
+            [2, 'Scatterplot Graph', '#scatterplot-graph', ScatterplotGraph],
+            [3, 'Heat Map', '#heat-map', HeatMap],
+            [4, 'Choropleth Map', '#choropleth-map', ChoroplethMap],
+            [5, 'Treemap Diagram', '#treemap-diagram', TreemapDiagram],
+          ] as unknown as NavArrayLayout
+        ).map(([index, title, url, project]) => (
+          <li key={index}>
+            <NavLink
+              firstOrLast={index.toString()}
+              href={url}
+              onClick={() => {
+                updateCurrentGraph({ project, title });
+                setExpandNav(false);
+              }}
+            >
+              {title}
+            </NavLink>
+          </li>
+        ))}
+      </NavListContainer>
+    </StyledNav>
+  );
+};
+
+/* -------------------- default props / queries / exports ------------------- */
+
+export default Dropdown;
--- src/structures/components/index.ts
+++ src/structures/components/index.ts
@@ -1,6 +1,7 @@
 // @index('./*.{ts,tsx}', (f) => `export { default as ${f.name} } from '${f.path}';`)
 export { default as BarChart } from './BarChart';
 export { default as ChoroplethMap } from './ChoroplethMap';
+export { default as Dropdown } from './Dropdown';
 export { default as HeatMap } from './HeatMap';
 export { default as HomePage } from './HomePage';
 export { default as ScatterplotGraph } from './ScatterplotGraph';
