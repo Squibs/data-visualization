52f554abf776564b769f59cde04f8f4c1e78c1a0
Squibs <z.r.holman+github@gmail.com>
2022-10-16 22:59:59 -0600

    Basic homepage.

--- src/structures/components/HomePage.tsx
+++ src/structures/components/HomePage.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 
 /* --------------------------------- styles --------------------------------- */
+
+const HomePageContainer = styled.div`
+  ${tw`max-w-screen-md m-auto`}
+
+  & a {
+    ${tw`text-linkcolor hover:text-cyan-400`}
+  }
+`;
 
 /* ---------------------------------- types --------------------------------- */
 
 /* -------------------------------- component ------------------------------- */
 
 const HomePage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <HomePageContainer>
+      <h1 tw="text-center text-2xl font-medium">About this Project</h1>
+      {/* prettier-ignore */}
+      <p tw="mt-8">
+        A single site that is a collection of all the
+        <a href="https://d3js.org/" target="_blank" rel="noreferrer"> D3 </a>
+        projects I created that are required to get the freeCodeCamp
+        <i> Data Visualization </i>
+        certification (
+          <a href="#">my certification</a>
+        ).
+      </p>
+      <br />
+      <p>
+        Each of these charts pulls in the data from various freeCodeCamp supplied GitHub endpoints /
+        repositories via fetch requests. The data is then processed through the use of D3 to provide
+        a visualization of the data.
+      </p>
+    </HomePageContainer>
+  );
 };
 
 /* -------------------- default props / queries / exports ------------------- */
