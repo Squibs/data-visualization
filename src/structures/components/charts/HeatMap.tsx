d53859f3fc49c8a82ffd64512ae86ee6d1651502
Squibs <z.r.holman+github@gmail.com>
2022-10-19 14:30:21 -0600

    Remove unused text element.

--- src/structures/components/charts/HeatMap.tsx
+++ src/structures/components/charts/HeatMap.tsx
@@ -247,9 +247,6 @@ const HeatMap = () => {
 
           <HeatMapContainer>
             <D3HeatMap ref={svgRef} preserveAspectRatio="xMinYMin meet">
-              <text className="y-axis-label" style={{ fill: '#9d9d9d' }}>
-                {/* <tspan>Time (in Minutes)</tspan> */}
-              </text>
               <g className="plot-area" />
               <g id="x-axis" />
               <g id="y-axis" />
