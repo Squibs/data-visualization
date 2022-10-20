1e22649c1f4db8ee57468c2dd479ca8882920d33
Squibs <z.r.holman+github@gmail.com>
2022-10-19 21:11:39 -0600

    Choropleth map complete.

--- src/structures/components/charts/ChoroplethMap.tsx
+++ src/structures/components/charts/ChoroplethMap.tsx
@@ -11,6 +11,15 @@ import fakeEducationData from '../../../data/data-backup-choropleth-map-(US Educ
 
 const ChoroplethMapPageContainer = styled.div`
   ${tw`w-full`}
+
+  .states {
+    fill: none;
+    ${tw`stroke-amber-700`}
+  }
+
+  .county:hover {
+    ${tw`fill-cyan-400`}
+  }
 `;
 const ChoroplethMapContainer = tw.div`w-full h-auto max-w-screen-xl m-auto`;
 const D3ChoroplethMap = tw.svg`w-full h-full`;
@@ -48,8 +57,6 @@ const ChoroplethMap = () => {
 
   const createChoroplethMap = (cData: any, eData: EducationData[]) => {
     const margin = 60;
-    const width = 960;
-    const height = 600;
 
     // svg - fixed size as that's what this map is pre-projected to fit
     d3.select(svgRef.current).attr('viewBox', `0 0 960 600`);
@@ -59,12 +66,15 @@ const ChoroplethMap = () => {
 
     const educationScale = d3.scaleLinear().domain([minEducation, maxEducation]).range([0, 260]);
 
+    // --------------------legend-------------------------
+
+    // tick values
     const colorScaleBreakpoints: number[] = [];
     for (let i = 0; i < 8; i += 1) {
       colorScaleBreakpoints.push(Number(minEducation + i * ((maxEducation - minEducation) / 8)));
     }
 
-    // legend
+    // create legend scale / ticks
     d3.select('#legend')
       .append('g')
       .attr('class', 'legend-scale')
@@ -78,6 +88,7 @@ const ChoroplethMap = () => {
       .select('.domain') // removes line connecting all ticks
       .remove();
 
+    // relate colors to ticks
     const educationColorScale = d3
       .scaleThreshold()
       .domain(colorScaleBreakpoints)
@@ -86,6 +97,7 @@ const ChoroplethMap = () => {
     // leaves legendData[0][0] & legendData[8][1] as undefined, so they don't get a width
     const legendData = educationColorScale.range().map((d) => educationColorScale.invertExtent(d));
 
+    // create legend colored squares
     d3.select('#legend')
       .append('g')
       .selectAll('rect')
@@ -98,10 +110,12 @@ const ChoroplethMap = () => {
       .attr('height', 15)
       .style('fill', (d) => educationColorScale(d[0]));
 
+    // move legend position
     d3.select('#legend').attr('transform', `translate(${625}, ${margin - 20})`);
 
     // ------------------------us map-----------------------------
 
+    // create us map
     d3.select('.plot-area')
       .append('g')
       .attr('class', 'counties')
@@ -115,11 +129,51 @@ const ChoroplethMap = () => {
         const result = eData.filter((obj) => obj.fips === d.id);
 
         if (result[0]) return educationColorScale(result[0].bachelorsOrHigher);
-
-        // no matching fips id in the data default 0%
-        return educationColorScale(0);
+        return educationColorScale(0); // no matching fips to id in the data so default 0%
       })
-      .attr('d', d3.geoPath() as any);
+      .attr('d', d3.geoPath() as any)
+      .attr('data-fips', (d: any) => d.id)
+      .attr('data-education', (d: any) => {
+        const result = eData.filter((obj) => obj.fips === d.id);
+
+        if (result[0]) return result[0].bachelorsOrHigher;
+        return 0; // no matching fips to id in the data so default 0%
+      })
+      .attr('data-county', (d: any) => eData.filter((obj) => obj.fips === d.id)[0].area_name)
+      .attr('data-state', (d: any) => eData.filter((obj) => obj.fips === d.id)[0].state);
+
+    // create state boundaries
+    d3.select('.plot-area')
+      .append('path')
+      .attr('class', 'states')
+      .datum(topojson.mesh(cData, cData.objects.states, (a, b) => a !== b))
+      .attr('d', d3.geoPath());
+
+    // ------------------------tooltip---------------------------
+    const tooltip = d3.select('#tooltip');
+
+    d3.selectAll('.county')
+      .on('mouseover', () => {
+        tooltip.style('display', 'block');
+      })
+      .on('mousemove', (e) => {
+        const tooltipElement = tooltip.node() as Element;
+        const mouseXOffset = tooltipElement.getBoundingClientRect().width / 2;
+        const mouseYOffset = tooltipElement.getBoundingClientRect().height + 10;
+        const ld = e.target.dataset; // local dataset
+
+        tooltip
+          .html(
+            `<small>
+              ${ld.county}, ${ld.state}<br>
+              ${ld.education}%
+            </small>`,
+          )
+          .attr('data-education', ld.education)
+          .style('left', `${e.pageX - mouseXOffset}px`)
+          .style('top', `${e.pageY - mouseYOffset}px`);
+      })
+      .on('mouseleave', () => tooltip.style('display', 'none'));
   };
 
   // fetch data from freeCodeCamp onMount
@@ -156,8 +210,11 @@ const ChoroplethMap = () => {
       {countyData && educationData && !loading && !error && (
         <>
           <h1 id="title" tw="text-center text-2xl font-medium">
-            CHART TITLE HERE
+            U.S. Educational Attainment
           </h1>
+          <h2 id="description" tw="text-center text-xl font-light">
+            Adults (aged 25 and older) with a bachelor&apos;s degree or higher (2010 - 2014)
+          </h2>
 
           <ChoroplethMapContainer>
             <D3ChoroplethMap ref={svgRef} preserveAspectRatio="xMinYMin meet">
@@ -186,7 +243,7 @@ const ChoroplethMap = () => {
               <i>
                 Data:
                 <a
-                  href="http://berkeleyearth.org/data/"
+                  href="https://www.ers.usda.gov/data-products/county-level-data-sets/download-data.aspx"
                   tw="text-linkcolor hover:text-cyan-400"
                   target="_blank"
                   rel="noreferrer"
