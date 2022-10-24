161e0ef0748a7b527bb451fd13ae54ef5a855b4f
Squibs <z.r.holman+github@gmail.com>
2022-10-24 00:27:39 -0600

    Additional seo.

--- src/structures/components/SEO.tsx
+++ src/structures/components/SEO.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import socialBanner from '../../images/url-thumbnail.png';
 
 /* ---------------------------------- types --------------------------------- */
 
@@ -36,10 +37,14 @@ const SEO = ({ description, title, children }: SEOProps) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:site_name" content="Data Visualization" />
+      <meta property="og:image" content={socialBanner} />
+      <meta property="og:locale" content="en_US" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:image" content={socialBanner} />
       {children}
     </>
   );
